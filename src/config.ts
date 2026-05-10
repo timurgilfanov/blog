78f824000c1b10d13f6115bcebf418b66e6cb889
Timur Gilfanov <timurgilfanov@gmail.com>
2026-05-10 13:58:22 +0400

    Migrate blog to AstroPaper

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://gilfanov.dev/", // replace this with your deployed domain
+  author: "Timur Gilfanov",
+  profile: "https://gilfanov.dev/",
+  desc: "Notes on Android Engineering, Architecture, and Product Development",
+  title: "Timur Gilfanov",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/timurgilfanov/blog/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
