bf2c7b5dc9aa1b90dd47397ce8fea244e604bde2
Timur Gilfanov <timurgilfanov@gmail.com>
2026-05-10 11:16:10 +0400

    Use gilfanov.dev domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://timurgilfanov.github.io/blog",
+  site: "https://gilfanov.dev",
   integrations: [mdx(), sitemap()],
   fonts: [
     {
