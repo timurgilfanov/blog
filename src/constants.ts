af25a1571ea54524ffa9d1a5f2af73af6350e7e5
Timur Gilfanov <timurgilfanov@gmail.com>
2026-05-10 14:31:39 +0400

    Set actual email

--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:timur@gilfanov.dev",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
